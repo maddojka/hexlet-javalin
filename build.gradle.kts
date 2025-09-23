a3041670e27f72e5036d4f92d53bec894b2c34a4
maddojka <yuriy.soroko@gmail.com>
2025-09-23 19:58:21 +0300

    hello world

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("java")
+    application
+}
+
+application {
+    mainClass.set("ru.soroko.hexlet.HelloWorld")
 }
 
 group = "ru.soroko"
@@ -12,8 +20,20 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("gg.jte:jte:3.1.9")
 }
 
 tasks.test {
     useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
---
+++ src/main/java/ru.soroko/hexlet/HelloWorld.java
@@ -0,0 +1,15 @@
+package ru.soroko.hexlet;
+
+import io.javalin.Javalin;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Создаем приложение
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        // Описываем, что загрузится по адресу /
+        app.get("/", ctx -> ctx.result("Hello World"));
+        app.start(8080); // Стартуем веб-сервер
+    }
+}
